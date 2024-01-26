55cbfb4e5c88667ab7be4b1ff45c6eed552bff4d
Noman Shamim <nomanshamim2004@gmail.com>
2024-01-26 18:06:07 +0500

    Working

---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import Home from './Home';
+import About from './About';
+import Login from './Login';
+import { Link } from 'react-router-dom';
+const { Header, Content, Footer } = Layout;
+// const items = [
+//     {
+//         label: Home,
+//         key: "/home"
+//     },
+//     {
+//         label: About,
+//         key: "/About"
+//     },
+//     {
+//         label: Login,
+//         key: "/login"
+//     }
+// ]
+const Navbar = () => {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+    return (
+        <Layout>
+            <Header
+                style={{
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 1,
+                    width: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+            >
+                <div className="demo-logo" />
+                {/* <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    items={items}
+                    style={{
+                        flex: 1,
+                        minWidth: 0,
+                    }}
+                /> */}
+                <div>
+                    <div style={{color : "white" , display : "flex" , gap : "30px"}}>
+                        <span style={{cursor : "pointer"}}><Link to="/home"> Home </Link></span>
+                        <span style={{cursor : "pointer"}}> <Link to="/About">About </Link></span>
+                        <span style={{cursor : "pointer"}}><Link to="/login"> Login </Link></span>
+                        <span style={{cursor : "pointer"}}> <Link to="/Signup">SignUp </Link></span>
+                        <span style={{cursor : "pointer"}}><Link to="/logout"> LogOut </Link></span>
+                    </div>
+                </div>
+            </Header>
+            <Content
+                style={{
+                    padding: '0 48px',
+                }}
+            >
+        
+                <div
+                    style={{
+                        padding: 24,
+                        minHeight: 380,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    Home
+                </div>
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                Ant Design ©{new Date().getFullYear()} Created by Ant UED
+            </Footer>
+        </Layout>
+    );
+};
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import Home from "./Pages/Home"
 import About from "./Pages/About"
 import Login from "./Pages/Login"
+import SignUp from './Pages/SignUp';
+import LogOut from './Pages/LogOut';
+import Navbar from './Pages/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Navbar/>}/>
+      <Route path='/home' element={<Home/>}/>
       <Route path='/About' element={<About/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/Signup' element={<SignUp/>}/>
+      <Route path='/logout' element={<LogOut/>}/>
 
     </Routes>
 
